76d2eae6ff74cca9599d4cf0f83fc420410e5cc6
Saliim242 <salim7442a@gmail.com>
2025-05-28 15:29:57 +0300

    Solved Password Hashing Issue

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,51 +2,63 @@ import User from "../models/user.model.js";
 import { hashPassword } from "../utils/password.hash.util.js";
 
 export const register = async (req, res) => {
-  const { fullName, username, email, password, role } = req.body;
+  try {
+    const { fullName, username, email, password, role } = req.body;
 
-  if (!fullName || !username || !email || !password || !role) {
-    return res.status(400).json({
+    if (!fullName || !username || !email || !password || !role) {
+      return res.status(400).json({
+        status: false,
+        message: "All fields are required",
+      });
+    }
+
+    const isUsernameExists = await User.findOne({ username });
+    if (isUsernameExists) {
+      res.status(400).json({
+        status: false,
+        message: `Username ${username} already exists`,
+      });
+    }
+
+    const isEmailExists = await User.findOne({ email });
+    if (isEmailExists) {
+      res
+        .status(400)
+        .json({ status: false, message: `Email ${email} already exists` });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = await User.create({
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        status: false,
+        message: "User registration failed",
+      });
+    }
+
+    res.status(200).json({
+      status: true,
+      message: "User registered successfully",
+      data: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
       status: false,
-      message: "All fields are required",
+      message: error.message,
     });
   }
-
-  const isUsernameExists = await User.findOne({ username });
-  if (isUsernameExists) {
-    res
-      .status(400)
-      .json({ status: false, message: `Username ${username} already exists` });
-  }
-
-  const isEmailExists = await User.findOne({ email });
-  if (isEmailExists) {
-    res
-      .status(400)
-      .json({ status: false, message: `Email ${email} already exists` });
-  }
-
-  const hashedPassword = hashPassword(password);
-
-  const user = await User.create({
-    fullName,
-    username,
-    email,
-    password: hashedPassword,
-    role,
-  });
-
-  if (!user) {
-    return res.status(400).json({
-      status: false,
-      message: "User registration failed",
-    });
-  }
-
-  res.status(200).json({
-    status: true,
-    message: "User registered successfully",
-    data: user,
-  });
 };
 
 export const login = async (req, res) => {
--- src/utils/password.hash.util.js
+++ src/utils/password.hash.util.js
@@ -1,8 +1,8 @@
 import bcrypt from "bcryptjs";
 
 export const hashPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
+  //   const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, 10);
 };
 
 export const comparePassword = async (password, hashPassword) => {
